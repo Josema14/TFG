b9ebf7df6fc2f676f8c3315416ec6481aa862208
josemaria <josemaespin14@gmail.com>
2022-06-29 11:17:07 +0200

    Commit inicial

---
+++ tfg/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+
+function App() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
