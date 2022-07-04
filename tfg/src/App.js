f4d52709a29f0c0d7a8b0d647b621e38c36596ae
josemaria <josemaespin14@gmail.com>
2022-07-04 15:41:51 +0200

    NavBar y tutorial router borrado

--- tfg/src/App.js
+++ tfg/src/App.js
@@ -1,17 +1,21 @@
 import React, {useState} from 'react';
-
-
+import LoginForm from './components/LoginForm';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css';
+import Home from './pages';
 function App() {
 
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123"
-  }
+  
   return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    /* Router Dom para moverse entre páginas*/
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" exact component={Home} />
+      </Routes>
+    </Router>
+  )
+      }
 
 export default App;
