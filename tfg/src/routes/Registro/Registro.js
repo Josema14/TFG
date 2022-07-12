70e898267f4ac44b38eea9f1351f27d83fc6fe60
josemaria <josemaespin14@gmail.com>
2022-07-12 15:01:18 +0200

    Registro conectado a la BD

--- tfg/src/routes/Registro/Registro.js
+++ tfg/src/routes/Registro/Registro.js
@@ -1,7 +1,44 @@
 import React, {useState} from 'react'
+import { useNavigate } from "react-router-dom";
 import "./Registro.css"
+
 function Registro({Login, error}) {
-    
+
+    const [form,setForm] = useState({
+        name: "",
+        email: "",
+        password:"",
+    })
+    const navigate = useNavigate();
+    //Método actualizar el estado
+
+    function updateForm(value){
+        return setForm((prev) => {
+            return {...prev,...value};
+        });
+    }
+
+    async function onSubmit(e){
+        e.preventDefault();
+        
+        const newUser = {...form};
+
+        await fetch("http://localhost:5000/record/add", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser),
+          })
+          .catch(error => {
+            window.alert(error);
+            return;
+          });
+        
+          setForm({ name: "", email: "", password: "" });
+          
+          navigate("/");
+        }
  
     return (
         <div className='container'>
@@ -16,14 +53,14 @@ function Registro({Login, error}) {
             </div>
 
             <div className='form-container'>
-                <form action='#'>
+                <form action='#' onSubmit={onSubmit}>
                     <h1>Registro</h1>
                     <div>
-                    <input type="text" placeholder="Nombre de Usuario" />
-                    <input type="email" placeholder="Email" />
-			        <input type="password" placeholder="Contraseña" />
+                    <input type="text" placeholder="Nombre de Usuario" value={form.name} onChange={(e) => updateForm({name: e.target.value})} />
+                    <input type="email" placeholder="Email" value={form.email} onChange={(e) => updateForm({email: e.target.value})} />
+			        <input type="password" placeholder="Contraseña" value={form.password} onChange={(e) => updateForm({password: e.target.value})} />
                     </div>
-			        <button>Confirmar</button>
+			        <button type='submit'>Confirmar</button>
                 </form>
 
             </div>
